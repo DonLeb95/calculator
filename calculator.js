34dbf4fc79b08d3cad2a2ecec5718dae0cd87267
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-03-06 00:56:02 -0400

    Create arithmetic functions and number buttons

---
+++ calculator.js
@@ -0,0 +1,34 @@
+//arithmetic functions
+
+function add(n1, n2) {
+    return n1 + n2
+}
+
+function subtract (n1, n2) {
+    return n1 - n2
+}
+
+function multiply (n1, n2) {
+    return n1 * n2
+}
+
+function divide (n1, n2) {
+    return n1 / n2
+}
+
+function operate (operator, num1, num2) {
+    switch(operator){
+        case "+" :
+            add(num1,num2);
+            break;
+        case "-" :
+            subtract(num1,num2);
+            break;
+        case "*" :
+            multiply(num1,num2);
+            break;
+        case "/" :
+            divide(num1,num2);
+            break;
+    }
+}
