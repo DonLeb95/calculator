d620ea26faf144f64349bd3f595c14b149d8ce8e
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-03-07 00:29:44 -0400

    Design the graphics of the calculator
    Added some extra buttons to the calculator, mostly the operator one. Modt of the commit is just applying CSS to the elements

--- calculator.js
+++ calculator.js
@@ -15,7 +15,9 @@ function multiply (n1, n2) {
 function divide (n1, n2) {
     return n1 / n2
 }
-
+let operator;
+let num1;
+let num2;
 function operate (operator, num1, num2) {
     switch(operator){
         case "+" :
