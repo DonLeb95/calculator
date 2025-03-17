c641233692bdab800b7832210d03ae4bad4bcfa0
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-03-16 23:20:12 -0400

    Remove earlier calculation code

--- calculator.js
+++ calculator.js
@@ -34,6 +34,7 @@ const display = document.querySelector(".calculator-display");
 
 document.querySelectorAll("button").forEach(button => {
     button.addEventListener("click", function () {
+        
         if (this.textContent.trim() == "C"){
             return display.textContent = ""
         } else if (this.textContent.trim() == "="){
@@ -46,21 +47,13 @@ document.querySelectorAll("button").forEach(button => {
                 return display.textContent = "You can't calculate that";
             }
 
-            //for operations that contain more than one operator
-            if (arr.length > 3){
-                let nums = arr.filter(numbers => !isNaN(numbers)).map(Number);
-                let oper = arr.filter(operators => isNaN(operators));
-                for(let i = 0;i < oper.length;i++){
-                    nums[i+1] = operate(oper[i],nums[i],nums[i+1]);   
-                }
-                return display.textContent = nums[nums.length - 1]; 
+            let nums = arr.filter(numbers => !isNaN(numbers)).map(Number);
+            let oper = arr.filter(operators => isNaN(operators));
+            for(let i = 0;i < oper.length;i++){
+                nums[i+1] = operate(oper[i],nums[i],nums[i+1]);   
             }
-
-            num1 = Number(arr[0]);
-            num2 = Number(arr[2]);
-            operator = arr[1];
-            return display.textContent = operate(operator,num1,num2);
-        }
+            return display.textContent = nums[nums.length - 1]; 
+            }
         display.textContent += this.textContent.trim();
     })                                                                               
 })
