4d410bc68fd8568ee1e75bd494b19e2150f5f7b2
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-03-21 00:50:20 -0400

    Add keyboard support
    Ts running a bit slowly for my liking but it did manage to get the keyboard to functon as needed

--- calculator.js
+++ calculator.js
@@ -85,3 +85,61 @@ document.querySelectorAll("button").forEach(button => {
         display.textContent += this.textContent.trim();
     })                                                                               
 })
+
+document.addEventListener("keyup", function (e) {
+        
+    const requiredKeys = ["Enter", "Backspace", "Delete", "+", "-", "*", "/", ".", "="];
+
+    if (!/\d/.test(e.key) && !requiredKeys.includes(e.key)) {
+        return;
+    }
+
+    if (e.key == "Delete"){
+        display.textContent = "";
+        check = false;
+        return;
+    }
+    
+    if (e.key == "Backspace"){
+        display.textContent = display.textContent.slice(0,-1);
+        check = false;
+        return;
+    }
+
+    if (e.key == "Enter"){
+        const arr = display.textContent.split(/([^\d.])/).filter(Boolean);
+
+        //in case of consecutive operator uses and no number post operator
+        const doubleOperatorCheck = arr.map((check,ind) => isNaN(check) && isNaN(arr[ind+1]));
+        const badCalc = doubleOperatorCheck.filter(Boolean);
+        if(badCalc.length > 0){
+            display.textContent = "You can't calculate that";
+            check = false;
+            return;
+        }
+
+        const division0Check = arr.map((check,ind) => check + arr[ind+1]);
+        const divBy0 = division0Check.filter(check => check == "/0")
+        if(divBy0.length > 0){
+            display.textContent = "That ain't it dawg";
+            check = false;
+            return;
+        }
+
+        let nums = arr.filter(numbers => !isNaN(numbers)).map(Number);
+        let oper = arr.filter(operators => isNaN(operators));
+        for(let i = 0;i < oper.length;i++){
+            nums[i+1] = operate(oper[i],nums[i],nums[i+1]);   
+        }
+        display.textContent = nums[nums.length - 1];
+        check = true;
+        return; 
+    }
+
+    if (check) {
+        display.textContent = "";
+        check = false;          
+    }
+     
+    display.textContent += e.key.trim();
+})
